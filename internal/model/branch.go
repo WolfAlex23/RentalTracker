0d14f527a9cbc02820f884c67fac96951a81b165
Александр Музуров <muzurov26@gmail.com>
2025-07-10 17:02:02 +0300

    добавлено описание

--- internal/model/branch.go
+++ internal/model/branch.go
@@ -7,8 +7,8 @@ type Branch struct {
 	Department  string // наименование филиала
 	Address     string // адрес филиала
 	Contract    string
-	Aria        float64 // занимаемая площадь
-	MeterInYear float64 // стоимость м2 в год
-	TotalInYear float64 // общая стоимость в год
-	UpdatedAt   time.Time
+	Aria        float64   // занимаемая площадь
+	MeterInYear float64   // стоимость м2 в год
+	TotalInYear float64   // общая стоимость в год
+	UpdatedAt   time.Time // дата последнего обновления или создания
 }

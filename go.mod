ce1449539dd9f610e6007ea53adbd24bca4fe54b
Александр Музуров <muzurov26@gmail.com>
2025-07-29 15:47:51 +0300

    style output

--- go.mod
+++ go.mod
@@ -5,10 +5,20 @@ go 1.24.3
 require modernc.org/sqlite v1.38.0
 
 require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/olekukonko/errors v1.1.0 // indirect
+	github.com/olekukonko/ll v0.0.9 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+)
+
+require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/olekukonko/tablewriter v1.0.9
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
--- internal/handlers/getBranches.go
+++ internal/handlers/getBranches.go
@@ -2,6 +2,11 @@ package handlers
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/renderer"
+	"github.com/olekukonko/tablewriter/tw"
 
 	"github.com/wolfalex23/rental-tracker/internal/data"
 )
@@ -14,20 +19,37 @@ func ListHandler() {
 		return
 	}
 
-	// Если филиалы найдены, выводим каждый филиал на экран
+	table := tablewriter.NewTable(os.Stdout,
+		tablewriter.WithRenderer(renderer.NewBlueprint(tw.Rendition{
+			Settings: tw.Settings{Separators: tw.Separators{BetweenRows: tw.On}},
+		})))
+
+	// Заголовки таблицы
+	header := []string{"ID", "Department", "Address", "Contract", "Aria", "MeterInYear", "TotalInYear", "UpdatedAt"}
+	table.Header(header)
+
+	var rows [][]string
+
 	for _, branch := range branches {
-		fmt.Printf("ID: %d, Department: %s, Address: %s, Contract: %s, Aria: %.2f, MeterInYear: %.2f, TotalInYear: %.2f, UpdatedAt: %s\\n",
-			branch.ID,
+		rows = append(rows, []string{
+			fmt.Sprint(branch.ID),
 			branch.Department,
 			branch.Address,
 			branch.Contract,
-			branch.Aria,
-			branch.MeterInYear,
-			branch.TotalInYear,
-			branch.UpdatedAt)
+			fmt.Sprintf("%.2f", branch.Aria),
+			fmt.Sprintf("%.2f", branch.MeterInYear),
+			fmt.Sprintf("%.2f", branch.TotalInYear),
+			branch.UpdatedAt.Format("2006-01-02 15:04:05"),
+		})
 	}
 
+	// Добавляем данные в таблицу одним махом
+	table.Bulk(rows)
+
+	// Рендерим таблицу
+	table.Render()
+
 	if len(branches) == 0 {
-		fmt.Println("Филиалы отсутствуют")
+		fmt.Println("Филиалы отсутствуют.")
 	}
 }
